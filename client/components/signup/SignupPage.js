e930d1b4788fcd47d25fd56c974b35f7fca6d7ea
anupmannem <anupmannem@gmail.com>
2019-04-16 20:39:44 +0530

    react-router setup

---
+++ client/components/signup/SignupPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class SignupPage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Sign up page</h1>
+      </div>
+    )
+  }
+}
